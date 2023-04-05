b97558dcdf74460ab8dd8ea032ab611565e39b74
Kamal Kishor Joshi <51081539+kamal-kishor@users.noreply.github.com>
2023-04-05 11:35:00 +0530

    Delete node_modules directory

--- node_modules/snyk-nodejs-lockfile-parser/dist/dep-graph-builders/npm-lock-v2/extract-npm-lock-v2-pkgs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.extractPkgsFromNpmLockV2 = void 0;
-const extractPkgsFromNpmLockV2 = (pkgLockContent) => {
-    return JSON.parse(pkgLockContent).packages;
-};
-exports.extractPkgsFromNpmLockV2 = extractPkgsFromNpmLockV2;
-//# sourceMappingURL=extract-npm-lock-v2-pkgs.js.map
