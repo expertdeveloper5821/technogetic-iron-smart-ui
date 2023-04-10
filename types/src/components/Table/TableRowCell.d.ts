f2e91373e950e104dc6f23897601501602671e12
Kamal-Joshi <kkjoshi.kk@gmail.com>
2023-04-10 13:11:37 +0530

    Table component are added

--- types/src/components/Table/TableRowCell.d.ts
+++ types/src/components/Table/TableRowCell.d.ts
@@ -1,7 +1,9 @@
+import React from 'react';
 import { IColumnType } from './Table';
 interface Props<T> {
     item: T;
     column: IColumnType<T>;
+    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
-declare const TableRowCell: <T extends Props<T>>({ item, column }: any) => JSX.Element;
+declare const TableRowCell: <T extends Props<T>>({ item, column, onClick }: any) => JSX.Element;
 export default TableRowCell;
