0d0ea91a59bfb9006c8469e1973f11a8ac74e0cd
Kamal-Joshi <kkjoshi.kk@gmail.com>
2023-05-22 16:58:02 +0530

    adding strorybook functionality on nav and drawer

--- src/stories/Drawer.stories.tsx
+++ src/stories/Drawer.stories.tsx
@@ -12,11 +12,89 @@ const Template: ComponentStory<typeof Drawer> = (args) => <Drawer {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
     align: 'left',
-    isOpen: true
+    isOpen: true,
+    children: (
+        <>
+            <MenuItem
+                onClick={(e) => {
+                    console.log(e);
+                }}
+            >
+                Item-1
+            </MenuItem>
+            <MenuItem
+                onClick={(e) => {
+                    console.log(e);
+                }}
+            >
+                Item-2
+            </MenuItem>
+            <MenuItem
+                onClick={(e) => {
+                    console.log(e);
+                }}
+            >
+                Item-3
+            </MenuItem>
+            <MenuItem
+                onClick={(e) => {
+                    console.log(e);
+                }}
+            >
+                Item-4
+            </MenuItem>
+            <MenuItem
+                onClick={(e) => {
+                    console.log(e);
+                }}
+            >
+                Item-5
+            </MenuItem>
+        </>
+    )
 };
 
 export const right = Template.bind({});
 right.args = {
     align: 'right',
-    isOpen: true
+    isOpen: true,
+    children: (
+        <>
+            <MenuItem
+                onClick={(e) => {
+                    console.log(e);
+                }}
+            >
+                Item-1
+            </MenuItem>
+            <MenuItem
+                onClick={(e) => {
+                    console.log(e);
+                }}
+            >
+                Item-2
+            </MenuItem>
+            <MenuItem
+                onClick={(e) => {
+                    console.log(e);
+                }}
+            >
+                Item-3
+            </MenuItem>
+            <MenuItem
+                onClick={(e) => {
+                    console.log(e);
+                }}
+            >
+                Item-4
+            </MenuItem>
+            <MenuItem
+                onClick={(e) => {
+                    console.log(e);
+                }}
+            >
+                Item-5
+            </MenuItem>
+        </>
+    )
 };
--- src/stories/NavBar.stories.tsx
+++ src/stories/NavBar.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { NavBar } from '../components/NavBar/NavBar';
+import { MenuItem } from '../components/MenuItem/MenuItem';
 
 export default {
     title: 'ReactComponentLibrary/NavBar',
@@ -12,10 +13,43 @@ const Template: ComponentStory<typeof NavBar> = (args) => <NavBar {...args} />;
 export const Primary = Template.bind({});
 
 Primary.args = {
-    // align: 'top'
-};
-export const Bottom = Template.bind({});
-
-Bottom.args = {
-    align: 'bottom'
+    children: (
+        <>
+            <MenuItem
+                onClick={(e) => {
+                    console.log(e);
+                }}
+            >
+                Item-1
+            </MenuItem>
+            <MenuItem
+                onClick={(e) => {
+                    console.log(e);
+                }}
+            >
+                Item-2
+            </MenuItem>
+            <MenuItem
+                onClick={(e) => {
+                    console.log(e);
+                }}
+            >
+                Item-3
+            </MenuItem>
+            <MenuItem
+                onClick={(e) => {
+                    console.log(e);
+                }}
+            >
+                Item-4
+            </MenuItem>
+            <MenuItem
+                onClick={(e) => {
+                    console.log(e);
+                }}
+            >
+                Item-5
+            </MenuItem>
+        </>
+    )
 };
